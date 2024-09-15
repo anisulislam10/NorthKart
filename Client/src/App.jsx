479bf33189aa6ec1b9722ef74980188e326e4044
Anisul Islam <anis.inbox10@gmail.com>
2024-09-15 04:03:20 -0700

    added footer and navbar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/shared/Navbar'
+import Footer from './components/shared/Footer'
 
 function App() {
 
   return (
     <div>
-     
-     <h1 className=' mt-[50px] mx-[35%] my-auto'>Welcome to Initial Stage of <span className='bg-red-400 mt-[100px] text-white px-2 rounded-md font-bold underline-offset-10'>NortKart - App</span></h1>
-     </div>
+     <Navbar/> 
+     <Footer/>
+          </div>
   )
 }
 
---
+++ Client/src/components/shared/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    
+    <div>
+
+    <div class="xxsml:ml-[5px] mt-5">
+    <ul class="xxsml:flex space-x-1 items-start ">
+  <li>CATALOGUE</li>
+  <li>FASHION</li>
+  <li>FAVOURITE</li>
+  <li>LIFESTYLE</li>
+  <li>SIGN UP</li>
+</ul>
+</div>
+      </div></>
+  )
+}
+
+export default Navbar
