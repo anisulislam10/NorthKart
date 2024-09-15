1bae44a751eadd0b42229d25f9ad0790fcf6ead2
Anisul Islam <anis.inbox10@gmail.com>
2024-09-15 04:02:53 -0700

    define screens for mob, tab, laptop

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -6,6 +6,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xxsml': '1px',
+      // => @media (min-width: 1px) { ... }
+
+      'xsml': '300px',
+      // => @media (min-width: 300px) { ... }
+
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
   	extend: {
   		borderRadius: {
   			lg: 'var(--radius)',
